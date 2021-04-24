93d96ac658a73f0c0cc10b22dab07d828f8e042e
trovtle basnet <basnet.trovtle@gmail.com>
2021-04-25 03:08:16 +1000

    Typo fixed

--- src/screens/Projects.js
+++ src/screens/Projects.js
@@ -68,7 +68,7 @@ function Projects({ theme }) {
 					/>
 					<Project
 						theme={theme}
-						projectName="Portfolio - MERN"
+						projectName="Portfolio"
 						projectGitLink="https://github.com/RioterTrov97/portfolio-site-2021"
 						projectDesc="The portfolio app is my personal project that displays my capabilities as a web dev. Developed with love using React, Node and Express."
 						projectLive="https://suman-basnet.web.app/"
